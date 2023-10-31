dec27f819dd4f3d83eac3a365ea2385c957cd91f
Jacob Lee <JLpython@outlook.com>
2023-10-31 00:30:50 -0700

    feat: define pynumcalc.differential.FiniteDifference class

--- pynumcalc/differential.py
+++ pynumcalc/differential.py
@@ -1,2 +1,47 @@
 """
 """
+
+import typing
+
+
+class FiniteDifference:
+    """
+    """
+    @staticmethod
+    def forward(f: typing.Callable[[float], float], h: float) -> typing.Callable[[float], float]:
+        r"""
+        .. math
+
+            {\Delta}_{h}[f](x) = f(x + h) - f(x)
+
+        :param f:
+        :param h:
+        :return:
+        """
+        return lambda x: f(x + h) - f(x)
+
+    @staticmethod
+    def backward(f: typing.Callable[[float], float], h: float) -> typing.Callable[[float], float]:
+        r"""
+        .. math
+
+            {\nabla}_{h}[f](x) = f(x) - f(x - h)
+
+        :param f:
+        :param h:
+        :return:
+        """
+        return lambda x: f(x) - f(x - h)
+
+    @staticmethod
+    def central(f: typing.Callable[[float], float], h: float) -> typing.Callable[[float], float]:
+        r"""
+        .. math
+
+            {\delta}_{h}[f](x) = f(x + \frac{h}{2}) - f(x - \frac{h}{2})
+
+        :param f:
+        :param h:
+        :return:
+        """
+        return lambda x: f(x + h / 2) - f(x - h / 2)
